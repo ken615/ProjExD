de1881fa9a55801278be134e0b96778f15fcdb0a
ken5 <c0a21030cd@edu.teu.ac.jp>
2022-06-22 01:26:50 +0900

    calc 2進数計算追加

--- ex02/calc.py
+++ ex02/calc.py
@@ -26,6 +26,17 @@ if __name__ == "__main__":
             b_res = b_res[2:]#先頭の0bをとる
             entry.delete(0, tk.END)
             entry.insert(tk.END, b_res)
+
+        #2-2がクリックされたとき
+        elif txt == "2-2":
+            eqn = entry.get()
+            oprnds = eqn.split("+")#+を区切り文字として左オペランドと右オペランドに分割する。
+            op1 = int(oprnds[0], 2)#左オペランドで、入力した2進数を10進数に直したもの
+            op2 = int(oprnds[1], 2)#右オペランドで、入力した2進数を10進数に直したもの
+            res = op1 + op2#10進数の計算
+            b_res = bin(res)#計算結果を2進数に変換
+            entry.delete(0, tk.END)#テキストを消去
+            entry.insert(tk.END, b_res[2:])#計算結果の2進数を表示
             
         #それ以外の時
         else:
@@ -69,11 +80,17 @@ if __name__ == "__main__":
     button.bind("<1>", button_click)
     button.grid(row = r + 1, column = 0)
 
-    #2進数計算のボタン
+    #計算結果を2進数にするボタン
     button = tk.Button(root, text = "bin+=", width = 4, height = 1, 
                            font = ("Times New Roman", 30))
     button.bind("<1>", button_click)
     button.grid(row = r + 1, column = 1)
+
+    #2進数同士の計算をするボタン
+    button = tk.Button(root, text = "2-2", width = 4, height = 1,
+                        font = ("Times New Roman", 30))
+    button.bind("<1>", button_click)
+    button.grid(row = r + 1, column = 2)
 
     #表示欄
     entry = tk.Entry(root, justify = "right", width = 10, font = ("Times New Roman", 40))
