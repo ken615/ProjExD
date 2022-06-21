736ac016f13683cee4f61a5ae894af3a99fa3a20
ken5 <c0a21030cd@edu.teu.ac.jp>
2022-06-21 15:48:25 +0900

    AC追加

--- ex02/calc.py
+++ ex02/calc.py
@@ -3,21 +3,26 @@ import tkinter.messagebox as tkm
 
 if __name__ == "__main__":
 
+#ボタンを押したときの関数
     def button_click(event):
         btn = event.widget
         txt = btn["text"]#クリックされたボタン
+        #=がクリックされたとき
         if txt == "=":
             eqn = entry.get()
             res = eval(eqn)
             entry.delete(0, tk.END)
             entry.insert(tk.END, res)
-        #tkm.showinfo("報告", f"{txt}のボタンがクリックされました。")
         else:
             entry.insert(tk.END, f"{txt}")
 
+        #ACがクリックされたとき
+        if txt == "AC":
+            entry.delete(0, tk.END)
+
     root = tk.Tk()
-    root.title("calc")
-    root.geometry("300x550")#ウィンドウの表示
+    root.title("すごそうな電卓")
+    #root.geometry("300x550")#ウィンドウの表示
 
     #grid()のための変数r=row, c=column
     r = 1
@@ -25,7 +30,7 @@ if __name__ == "__main__":
 
     #数字のボタン
     for i in range(9, -1, -1):
-        button = tk.Button(root, text = f"{i}", width = 4, height = 2, 
+        button = tk.Button(root, text = f"{i}", width = 4, height = 1, 
                            font = ("Times New Roman", 30))
         button.bind("<1>", button_click)
         button.grid(row = r, column = c)
@@ -35,16 +40,22 @@ if __name__ == "__main__":
             c = 0
     
     #+のボタン
-    button = tk.Button(root, text = "+", width = 4, height = 2, 
+    button = tk.Button(root, text = "+", width = 4, height = 1, 
                            font = ("Times New Roman", 30))
     button.bind("<1>", button_click)
     button.grid(row = r, column = c)
 
     #=のボタン
-    button = tk.Button(root, text = "=", width = 4, height = 2, 
+    button = tk.Button(root, text = "=", width = 4, height = 1, 
                            font = ("Times New Roman", 30))
     button.bind("<1>", button_click)
     button.grid(row = r, column = c + 1)
+
+    #AC(オールクリア)のボタン
+    button = tk.Button(root, text = "AC", width = 4, height = 1, 
+                           font = ("Times New Roman", 30))
+    button.bind("<1>", button_click)
+    button.grid(row = r + 1, column = 0)
 
     #表示欄
     entry = tk.Entry(root, justify = "right", width = 10, font = ("Times New Roman", 40))
