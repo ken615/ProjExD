8bee496414d027aec869714223b0a4a90be5ffd7
ken5 <c0a21030cd@edu.teu.ac.jp>
2022-06-21 14:59:46 +0900

    calc4

--- ex02/calc.py
+++ ex02/calc.py
@@ -11,9 +11,9 @@ if __name__ == "__main__":
 
     root = tk.Tk()
     root.title("calc")
-    root.geometry("300x500")#ウィンドウの表示
+    root.geometry("300x550")#ウィンドウの表示
 
-    r = 0
+    r = 1
     c = 0
     for i in range(9, -1, -1):
         button = tk.Button(root, text = f"{i}", width = 4, height = 2, 
@@ -21,9 +21,12 @@ if __name__ == "__main__":
         button.bind("<1>", button_click)
         button.grid(row = r, column = c)
         c += 1
-
         if ((i - 1) % 3) == 0:
             r += 1
             c = 0
+
+    entry = tk.Entry(root, width = 10, font = ("Times New Roman", 40))
+    entry.grid(row = 0, column = 0, columnspan = 3)
+
 
     root.mainloop()
