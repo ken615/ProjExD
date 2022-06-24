bc3d7f7481ce180298efc08819330e4f772afb1d
ken5 <c0a21030cd@edu.teu.ac.jp>
2022-06-24 21:37:34 +0900

    いろいろ追加して修正

--- ex02/calc.py
+++ ex02/calc.py
@@ -9,10 +9,13 @@ if __name__ == "__main__":
         txt = btn["text"]#クリックされたボタン
         #=がクリックされたとき
         if txt == "=":
-            eqn = entry.get()
-            res = eval(eqn)
-            entry.delete(0, tk.END)
-            entry.insert(tk.END, res)
+            try:
+                eqn = entry.get()
+                res = eval(eqn)
+                entry.delete(0, tk.END)
+                entry.insert(tk.END, res)
+            except SyntaxError:
+                pass
 
         #ACがクリックされたとき
         elif txt == "AC":
@@ -20,23 +23,29 @@ if __name__ == "__main__":
 
         #bin+=がクリックされたとき
         elif txt == "bin+=":
-            eqn = entry.get()
-            res = eval(eqn)
-            b_res = bin(res)#足し算の結果を2進数にする
-            b_res = b_res[2:]#先頭の0bをとる
-            entry.delete(0, tk.END)
-            entry.insert(tk.END, b_res)
+            try:
+                eqn = entry.get()
+                res = eval(eqn)
+                b_res = bin(res)#足し算の結果を2進数にする
+                b_res = b_res[2:]#先頭の0bをとる
+                entry.delete(0, tk.END)
+                entry.insert(tk.END, b_res)
+            except SyntaxError:
+                pass
 
-        #2-2がクリックされたとき
-        elif txt == "2-2":
-            eqn = entry.get()
-            oprnds = eqn.split("+")#+を区切り文字として左オペランドと右オペランドに分割する。
-            op1 = int(oprnds[0], 2)#左オペランドで、入力した2進数を10進数に直したもの
-            op2 = int(oprnds[1], 2)#右オペランドで、入力した2進数を10進数に直したもの
-            res = op1 + op2#10進数の計算
-            b_res = bin(res)#計算結果を2進数に変換
-            entry.delete(0, tk.END)#テキストを消去
-            entry.insert(tk.END, b_res[2:])#計算結果の2進数を表示
+        #bin=がクリックされたとき
+        elif txt == "bin=":
+            try:
+                eqn = entry.get()
+                oprnds = eqn.split("+")#+を区切り文字として左オペランドと右オペランドに分割する。
+                op1 = int(oprnds[0], 2)#左オペランドで、入力した2進数を10進数に直したもの
+                op2 = int(oprnds[1], 2)#右オペランドで、入力した2進数を10進数に直したもの
+                res = op1 + op2#10進数の計算
+                b_res = bin(res)#計算結果を2進数に変換
+                entry.delete(0, tk.END)#テキストを消去
+                entry.insert(tk.END, b_res[2:])#計算結果の2進数を表示
+            except ValueError:
+                pass
             
         #それ以外の時
         else:
@@ -55,66 +64,29 @@ if __name__ == "__main__":
     root.title("すごそうな電卓")
     #root.geometry("300x550")#ウィンドウの表示
 
-    #grid()のための変数r=row, c=column
+    #ボタンの作成
     r = 1
     c = 0
-
-    #数字のボタン
-    for i in range(9, -1, -1):
-        button = tk.Button(root, text = f"{i}", width = 4, height = 1, 
-                           font = ("Times New Roman", 30))
-        button.bind("<1>", button_click)
-        button.bind("<Enter>", mouse_on)
-        button.bind("<Leave>", mouse_out)
+    display = [9, 8, 7, "AC", 6, 5, 4, "-", 3, 2, 1, "+", 0, "bin+=", "bin=", "="]
+    for i, d in enumerate(display, 0):
+        button = tk.Button(root,
+                           text = f"{d}",
+                           width = 4,
+                           height = 1,
+                           font = ("Times New Roman", 30)
+                           )
+        button.bind("<1>", button_click)#ボタンをクリックしたとき
+        button.bind("<Enter>", mouse_on)#マウスカーソルがボタンの上に来た時
+        button.bind("<Leave>", mouse_out)#マウスカーソルがボタンから離れたとき
         button.grid(row = r, column = c)
         c += 1
-        if ((i - 1) % 3) == 0:
-            r += 1
+        if (i + 1) % 4 == 0:
             c = 0
-    
-    #+のボタン
-    button = tk.Button(root, text = "+", width = 4, height = 1, 
-                           font = ("Times New Roman", 30))
-    button.bind("<1>", button_click)
-    button.bind("<Enter>", mouse_on)
-    button.bind("<Leave>", mouse_out)
-    button.grid(row = r, column = c)
-
-    #=のボタン
-    button = tk.Button(root, text = "=", width = 4, height = 1, 
-                           font = ("Times New Roman", 30))
-    button.bind("<1>", button_click)
-    button.bind("<Enter>", mouse_on)
-    button.bind("<Leave>", mouse_out)
-    button.grid(row = r, column = c + 1)
-
-    #AC(オールクリア)のボタン
-    button = tk.Button(root, text = "AC", width = 4, height = 1, 
-                           font = ("Times New Roman", 30))
-    button.bind("<1>", button_click)
-    button.bind("<Enter>", mouse_on)
-    button.bind("<Leave>", mouse_out)
-    button.grid(row = r + 1, column = 0)
-
-    #計算結果を2進数にするボタン
-    button = tk.Button(root, text = "bin+=", width = 4, height = 1, 
-                           font = ("Times New Roman", 30))
-    button.bind("<1>", button_click)
-    button.bind("<Enter>", mouse_on)
-    button.bind("<Leave>", mouse_out)
-    button.grid(row = r + 1, column = 1)
-
-    #2進数同士の計算をするボタン
-    button = tk.Button(root, text = "2-2", width = 4, height = 1,
-                        font = ("Times New Roman", 30))
-    button.bind("<1>", button_click)
-    button.bind("<Enter>", mouse_on)
-    button.bind("<Leave>", mouse_out)
-    button.grid(row = r + 1, column = 2)
+            r += 1
 
     #表示欄
-    entry = tk.Entry(root, justify = "right", width = 10, font = ("Times New Roman", 40))
-    entry.grid(row = 0, column = 0, columnspan = 3)
+    entry = tk.Entry(root, justify = "right", width = 12, font = ("Times New Roman", 40))
+    entry.grid(row = 0, column = 0, columnspan = 5)
 
 
     root.mainloop()
