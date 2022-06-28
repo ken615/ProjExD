03930bc470b0cb3e8ffcf439e2157fe9c59df6be
ken5 <c0a21030cd@edu.teu.ac.jp>
2022-06-28 14:37:08 +0900

    maze5,6

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,8 +1,21 @@
 import tkinter as tk
 
+
+def key_down(event):
+    global key
+    key = event.keysym
+
+
+def key_up(event):
+    global key
+    key = ""
+
+
 if __name__ == "__main__":
     key = ""
     root = tk.Tk()
+    root.bind("<KeyPress>", key_down)
+    root.bind("<KeyRelease>", key_up)
     root.title("迷えるこうかとん")
 
     canvas = tk.Canvas(root,
