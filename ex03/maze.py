1ce04fada3427a99f6e868cdcd7f0efb0c8090cb
ken5 <c0a21030cd@edu.teu.ac.jp>
2022-06-28 15:12:17 +0900

    maze9

--- ex03/maze.py
+++ ex03/maze.py
@@ -33,15 +33,18 @@ if __name__ == "__main__":
     root.bind("<KeyRelease>", key_up)
     root.after(100, main_proc)
     root.title("迷えるこうかとん")
-
+    #背景
     canvas = tk.Canvas(root,
                        width = 1500,
                        height = 900,
                        bg = "black"
                       )
     canvas.pack()
+    #迷路作成
+    mz = mzmk.make_maze(15, 9)
+    #こうかとん生成
     koka = tk.PhotoImage(file = "ex03/fig/3.png")
     cx, cy = 300, 400
     canvas.create_image(cx, cy, image = koka, tag = "koka")
-
+    #ウィンドウ生成
     root.mainloop()
