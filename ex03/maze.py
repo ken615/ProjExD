92ca8395c46ed5a643a21b3aba2938f642214d73
ken5 <c0a21030cd@edu.teu.ac.jp>
2022-06-28 14:31:41 +0900

    maze3

--- ex03/maze.py
+++ ex03/maze.py
@@ -2,10 +2,16 @@ import tkinter as tk
 
 if __name__ == "__main__":
     root = tk.Tk()
+    root.title("迷えるこうかとん")
+
     canvas = tk.Canvas(root,
                        width = 1500,
                        height = 900,
                        bg = "black"
                       )
     canvas.pack()
+    koka = tk.PhotoImage(file = "ex03/fig/3.png")
+    cx, cy = 300, 400
+    canvas.create_image(cx, cy, image = koka, tag = "koka")
+
     root.mainloop()
