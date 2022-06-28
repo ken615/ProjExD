3da2523396d2f62cfdc311414c3a4683508924c4
ken5 <c0a21030cd@edu.teu.ac.jp>
2022-06-28 14:16:47 +0900

    maze2

--- ex03/maze.py
+++ ex03/maze.py
@@ -2,4 +2,10 @@ import tkinter as tk
 
 if __name__ == "__main__":
     root = tk.Tk()
+    canvas = tk.Canvas(root,
+                       width = 1500,
+                       height = 900,
+                       bg = "black"
+                      )
+    canvas.pack()
     root.mainloop()
