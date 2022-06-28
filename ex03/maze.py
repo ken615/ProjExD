af6844ab46b6b9aa7b3aaeb98d38dd1a8a45b088
ken5 <c0a21030cd@edu.teu.ac.jp>
2022-06-28 15:02:08 +0900

    maze7

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,21 +1,36 @@
 import tkinter as tk
 
-
+#キーが押されたとき
 def key_down(event):
     global key
     key = event.keysym
 
-
+#キーが離れたとき
 def key_up(event):
     global key
     key = ""
 
+#キーが押されたときのこうかとんの動き
+def main_proc():
+    global cx, cy
+    if key == "Up":
+        cy -= 20
+    if key == "Down":
+        cy += 20
+    if key == "Left":
+        cx -= 20
+    if key == "Right":
+        cx += 20
+    canvas.coords("koka", cx + 20, cy)
+    root.after(100, main_proc)
 
+#本文
 if __name__ == "__main__":
     key = ""
     root = tk.Tk()
     root.bind("<KeyPress>", key_down)
     root.bind("<KeyRelease>", key_up)
+    root.after(100, main_proc)
     root.title("迷えるこうかとん")
 
     canvas = tk.Canvas(root,
